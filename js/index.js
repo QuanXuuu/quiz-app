3a1cd1cc32048097e755c074f8dbc6d7f34f8eb3
Quan Xu <xuquan69220@gmail.com>
2022-05-16 23:08:32 +0200

    Add js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("[data-js='button']");
+
+function showAnswer() {
+  const answer = document.querySelector("[data-js='answer']");
+  answer.classList.toggle("card__answer");
+
+  if (this.innerText === "Show Answer") {
+    this.innerText = "Hide Answer";
+  } else {
+    this.innerText = "Show Answer";
+  }
+}
+
+button.addEventListener("click", showAnswer);
